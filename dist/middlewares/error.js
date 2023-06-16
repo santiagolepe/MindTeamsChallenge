e51d10e682dac60fe9d835c099da86ea7b0c56b5
Octavio Santiago <santiagolepe@gmail.com>
2023-06-16 12:03:05 -0600

    test only

--- dist/middlewares/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMiddleware = void 0;
-const errorMiddleware = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: "Internal server error" });
-};
-exports.errorMiddleware = errorMiddleware;
-//# sourceMappingURL=error.js.map
